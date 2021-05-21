8f12b7b2af2c04506e01fb602079cee4b8a188e6
DaegyeomHa <combbm@naver.com>
2021-05-21 23:05:02 +0900

    Delete unused import code

--- react/210521/src/App.tsx
+++ react/210521/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { setInterval } from "timers";
+
 import Text from "./Text";
 
 const App: React.FC = () => {
--- react/210521/src/Text.tsx
+++ react/210521/src/Text.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
 
 interface TextProps {
   readonly color: string;
